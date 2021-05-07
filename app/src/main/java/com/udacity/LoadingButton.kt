ffc8d4934cf3948909fe707ea1a71fc4716bb824
Sayyid A Naqvi <naqvi.sdsu@gmail.com>
2021-05-07 12:37:27 -0500

    changed onDraw(), work is still in progress

--- app/src/main/java/com/udacity/LoadingButton.kt
+++ app/src/main/java/com/udacity/LoadingButton.kt
@@ -12,6 +12,8 @@ import kotlin.properties.Delegates
 class LoadingButton @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
+
+    private lateinit var ovel: RectF
     private var widthSize = 0
     private var heightSize = 0
 
@@ -22,24 +24,28 @@ class LoadingButton @JvmOverloads constructor(
         when (new) {
             ButtonState.Loading -> {
                 // Do some action.
-                valueAnimator.animatedValue
+                valueAnimator.animatedFraction
+
             }
             ButtonState.Completed -> {
                 valueAnimator.cancel()
+            }
+            else -> {
+                valueAnimator.currentPlayTime
             }
         }
 
     }
 
     init {
-
+        ovel = RectF(60.25f, 400f, 130f, 148f)
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
     }
 
-    val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL
         textAlign = Paint.Align.CENTER
         textSize = 55.0f
@@ -50,7 +56,11 @@ class LoadingButton @JvmOverloads constructor(
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
-        paint.color = if (buttonState == ButtonState.Completed) Color.GRAY else Color.GREEN
+        paint.color = if (buttonState == ButtonState.Completed) Color.GREEN else Color.CYAN     // it changes color but not sure if logic is right
+        canvas?.drawArc(ovel, 430.0f, 222.5f, true, paint)
+        canvas?.drawText("Here is download button", 4.5f, 155.9f, paint)
+
+//        paint.color = if (buttonState == ButtonState.Completed) Color.GRAY else Color.GREEN
 
     }
 
